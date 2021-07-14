ffcf87f68433143165aae81362e68edea3afbd02
0603Adriano <0603adriano@gmail.com>
2021-07-13 22:52:18 -0300

    Created TesteSet.kt and moved data class Funcionario from TesteList.kt to Funcionario.class

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -3,9 +3,9 @@ package one.digitalinnovation.collections
 fun main() {
     val joao = Funcionario("João", 3000.0, "CLT")
     val maria = Funcionario("Maria", 2000.0, "PJ")
-    val rodrigo = Funcionario("Rodrigo", 1250.0, "CLT")
+    val pedro = Funcionario("Pedro", 1250.0, "CLT")
 
-    val funcionarios = listOf(joao, maria, rodrigo)
+    val funcionarios = listOf(joao, maria, pedro)
 
     funcionarios.forEach { println(it) }
     println("-----------------------------")
@@ -17,16 +17,4 @@ fun main() {
     println("-----------------------------")
 
     funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) }
-}
-
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContratacao: String
-) {
-    override fun toString(): String =
-        """
-            Nome: $nome
-            Salário: $salario
-        """.trimIndent()
 }
