ba28910f30ec719553005ee91ef90c366259ea8e
0603Adriano <0603adriano@gmail.com>
2021-07-13 22:25:07 -0300

    Created tests for sorteBy and groupBy

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val joao = Funcionario("João", 1250.0)
-    val maria = Funcionario("Maria", 2000.0)
-    val rodrigo = Funcionario("Rodrigo", 3000.0)
+    val joao = Funcionario("João", 3000.0, "CLT")
+    val maria = Funcionario("Maria", 2000.0, "PJ")
+    val rodrigo = Funcionario("Rodrigo", 1250.0, "CLT")
 
     val funcionarios = listOf(joao, maria, rodrigo)
 
@@ -11,11 +11,18 @@ fun main() {
     println("-----------------------------")
 
     println(funcionarios.find { it.nome == "Maria" })
+    println("-----------------------------")
+
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+    println("-----------------------------")
+
+    funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) }
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
         """
